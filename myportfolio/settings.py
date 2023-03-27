06bb57761d101ed0cef3f49ee455006db2fafd7b
Rahul Kumar <rahuliitbhu18@gmail.com>
2023-03-28 01:44:12 +0530

    first commit

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db._sqlite3',
     }
 }
 
